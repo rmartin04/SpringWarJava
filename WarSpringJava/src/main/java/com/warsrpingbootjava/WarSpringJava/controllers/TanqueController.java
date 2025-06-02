30ce12b03fb25586ac2ecb95e22c4075e5be2eca
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-06-02 19:43:07 +0200

    autowired en todos los controladores

--- WarSpringJava/src/main/java/com/warsrpingbootjava/WarSpringJava/controllers/TanqueController.java
+++ WarSpringJava/src/main/java/com/warsrpingbootjava/WarSpringJava/controllers/TanqueController.java
@@ -1,7 +1,13 @@
 package com.warsrpingbootjava.WarSpringJava.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.warsrpingbootjava.WarSpringJava.service.JavaWarService;
 
 @Controller
 public class TanqueController {
+	
+	@Autowired
+	private JavaWarService warService;
 
 }
--- WarSpringJava/src/main/java/com/warsrpingbootjava/WarSpringJava/controllers/UsuarioController.java
+++ WarSpringJava/src/main/java/com/warsrpingbootjava/WarSpringJava/controllers/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.warsrpingbootjava.WarSpringJava.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.warsrpingbootjava.WarSpringJava.service.JavaWarService;
 
 @Controller
 public class UsuarioController {
+	
+	@Autowired
+	private JavaWarService warService;
 
 }
